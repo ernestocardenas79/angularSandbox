472ec059ea6b7bdd4521baaa18e30859a3563ef9
Ernesto cardenas <rnst79@DESKTOP-D2H79NF.localdomain>
2019-10-18 08:39:18 -0600

    generando componentes principales par UI

---
+++ projects/ingresosEgresos/src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fhu-estadistica',
+  templateUrl: './estadistica.component.html',
+  styleUrls: ['./estadistica.component.scss']
+})
+export class EstadisticaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/ingresosEgresos/src/app/share/sidebar/sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fhu-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
