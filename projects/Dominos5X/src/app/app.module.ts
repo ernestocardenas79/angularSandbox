3ed4c32d12e461faefed9c225579bab58273ef95
niSoft <ernesto.cardenas79@gmail.com>
2019-12-12 21:58:34 -0700

    Layout de la aplicacion

--- projects/Dominos5X/src/app/app.module.ts
+++ projects/Dominos5X/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameConfigComponent } from './game-config/game-config.component';
+import { GamingScoreComponent } from './gaming-score/gaming-score.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameConfigComponent,
+    GamingScoreComponent
   ],
   imports: [
     BrowserModule,
