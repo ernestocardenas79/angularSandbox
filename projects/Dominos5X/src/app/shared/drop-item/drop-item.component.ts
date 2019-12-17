e0fe9127be0ccc9c2a3d4b7a3d04d71b4cd18a3c
Ernesto Cardenas Llanes <ernesto.llanes@neoris.com>
2019-12-16 18:24:24 -0700

    Se crea interaccion para numero de jugadores

---
+++ projects/Dominos5X/src/app/shared/drop-item/drop-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drop-item',
+  templateUrl: './drop-item.component.html',
+  styleUrls: ['./drop-item.component.scss']
+})
+export class DropItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/Dominos5X/src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DropItemComponent } from './drop-item/drop-item.component';
+
+@NgModule({
+    declarations: [DropItemComponent],
+    imports: [CommonModule],
+    exports: [DropItemComponent],
+    entryComponents: [DropItemComponent],
+})
+export class SharedModule {}
