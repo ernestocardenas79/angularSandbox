38bca3acc92e3dc1724635419860dd6665242cc8
Ernesto Cardenas Llanes <ernesto.llanes@neoris.com>
2019-12-17 10:13:52 -0700

    Creando componente de configuracion

--- projects/Dominos5X/src/app/app.module.ts
+++ projects/Dominos5X/src/app/app.module.ts
@@ -7,20 +7,18 @@ import { GameConfigComponent } from './game-config/game-config.component';
 import { GamingScoreComponent } from './gaming-score/gaming-score.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    GameConfigComponent,
-    GamingScoreComponent
-  ],
-  imports: [
-    CommonModule,
-    BrowserModule,
-    ReactiveFormsModule,
-    AppRoutingModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, GameConfigComponent, GamingScoreComponent],
+    imports: [
+        BrowserModule,
+        CommonModule,
+        ReactiveFormsModule,
+        AppRoutingModule,
+        SharedModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- projects/Dominos5X/src/app/game-config/game-config.component.ts
+++ projects/Dominos5X/src/app/game-config/game-config.component.ts
@@ -1,28 +1,70 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormArray, FormControl, Validators, FormGroup } from '@angular/forms';
+import {
+    Component,
+    OnInit,
+    ViewChild,
+    ViewContainerRef,
+    AfterViewInit,
+    ComponentFactoryResolver,
+} from '@angular/core';
+import {
+    FormBuilder,
+    FormControl,
+    Validators,
+    FormGroup,
+    FormArray,
+} from '@angular/forms';
+import { DropItemComponent } from '../shared/drop-item/drop-item.component';
 
 @Component({
-  selector: 'app-game-config',
-  templateUrl: './game-config.component.html',
-  styleUrls: ['./game-config.component.scss']
+    selector: 'app-game-config',
+    templateUrl: './game-config.component.html',
+    styleUrls: ['./game-config.component.scss'],
 })
-export class GameConfigComponent implements OnInit {
+export class GameConfigComponent implements OnInit, AfterViewInit {
+    constructor(
+        private formBuilder: FormBuilder,
+        private cfr: ComponentFactoryResolver
+    ) {}
 
-  configuracion: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
+    get players(): FormArray {
+        return this.configuracion.get('players') as FormArray;
+    }
+    configuracion: FormGroup;
+    limitOfPlayers = 4;
 
-  }
+    @ViewChild('playersContainer', { static: false, read: ViewContainerRef })
+    _vcr: ViewContainerRef;
 
-  ngOnInit() {
-    this.configuracion = this.formBuilder.group({
-      players: this.formBuilder.array([
-              new FormControl('', Validators.required),
-              new FormControl('', Validators.required),
-          ])
-    });
-  }
+    ngOnInit() {
+        this.configuracion = this.formBuilder.group({
+            winScore: [''],
+            players: this.formBuilder.array([
+                new FormControl('', Validators.required),
+                new FormControl('', Validators.required),
+            ]),
+        });
+    }
 
-  AddPlayer() {
-    // this.players.push(new FormControl('', Validators.required));
-  }
+    AddPlayer() {
+        if (this.players.controls.length < 4) {
+            this.players.push(new FormControl('', Validators.required));
+        } else {
+            alert('El numero maximo de jugadores es 4');
+        }
+
+        const factory = this.cfr.resolveComponentFactory(DropItemComponent);
+        // 1
+        // let containerRef = this._vcr.createComponent(factory);
+        // const viewRef = this._vcr.get(this._vcr.length - 1);
+        // viewRef.detach();
+        // containerRef = null;
+
+        // 2
+        // const component = factory.create(this._vcr.parentInjector);
+        // this._vcr.insert(component.hostView);
+    }
+
+    ngAfterViewInit() {
+        console.log('ngAfterViewInit', this._vcr);
+    }
 }
