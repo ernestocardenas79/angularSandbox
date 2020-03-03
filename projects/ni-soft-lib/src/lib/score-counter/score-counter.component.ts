8d1a890401344ffcdae0e23698b19050accb312a
Ernesto Cardenas Llanes <ernesto.cardenas79@gmail.com>
2020-03-03 08:14:24 -0700

    eliminando codigo comentado en Score Counter

--- projects/ni-soft-lib/src/lib/score-counter/score-counter.component.ts
+++ projects/ni-soft-lib/src/lib/score-counter/score-counter.component.ts
@@ -14,7 +14,6 @@ import {
     changeDetection: ChangeDetectionStrategy.Default,
 })
 export class ScoreCounterComponent implements OnInit {
-    puntosList: string[];
     completed = false;
     private maxSlots = 10;
     assignedSlots = 0;
@@ -25,21 +24,6 @@ export class ScoreCounterComponent implements OnInit {
     assignSlots(ptos: number) {
         console.log('assignSlots', ptos, this.assignedSlots);
         if (!this.completed) {
-            // let slotForAssign = ptos;
-
-            // if (this.assignedSlots + ptos >= this.maxSlots) {
-            //     this.completed = true;
-            //     slotForAssign = this.SlotsToAssing;
-
-            //     if (this.assignedSlots + ptos >= this.maxSlots) {
-            //         this.isCompleted.emit(true);
-            //     }
-            //     this.assignedSlots = this.maxSlots;
-            // } else {
-            //     this.assignedSlots += ptos;
-            // }
-            // this.incrementSlots(slotForAssign);
-
             if (this.assignedSlots + ptos >= this.maxSlots) {
                 if (this.assignedSlots + ptos >= this.maxSlots) {
                     this.isCompleted.emit(true);
@@ -49,27 +33,6 @@ export class ScoreCounterComponent implements OnInit {
                 this.assignedSlots += ptos;
             }
         }
-    }
-
-    get SlotsToAssing() {
-        return this.maxSlots - this.assignedSlots;
-    }
-
-    private incrementSlots(ptos: number) {
-        let aux = '';
-
-        if (this.puntosList?.length > 0) {
-            aux = this.puntosList.join('|');
-            for (let i = 0; i < ptos; i++) {
-                aux += '|a';
-            }
-            this.puntosList = aux.split('|');
-            console.log('aux', aux);
-        } else {
-            this.puntosList = ['a'];
-        }
-
-        console.log('incrementSlots', ptos, aux, this.puntosList);
     }
 
     constructor() {}
