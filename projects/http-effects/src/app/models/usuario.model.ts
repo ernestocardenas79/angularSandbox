73dfe0d28b1d7432b97073188798d617fabc4afc
calel <ernesto.cardenas79@gmail.com>
2021-04-04 20:36:00 -0600

    migrando a angular 11

--- projects/http-effects/src/app/models/usuario.model.ts
+++
@@ -1,9 +0,0 @@
-export class UsuarioModel{
-    
-    constructor(
-        private id: number,
-        private firstName: string,
-        private lastName: string,
-        private email:string,
-        private avatar:string){}
-}
